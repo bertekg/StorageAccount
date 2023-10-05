e9992756c18390be9982dd5ae59122a5fd69170f
bertekg <bartek.grywalski@gmail.com>
2023-10-05 20:25:25 +0200

    Add project files.

---
+++ StorageAccount/Controllers/BlobStorageController.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StorageAccount.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BlobStorageController : ControllerBase
+{
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload(IFormFile file)
+    {
+        var connctionString = "Tutaj Connction String";
+        BlobServiceClient blobServiceClient = new BlobServiceClient(connctionString);
+
+        var containerName = "documents";
+
+        BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+
+    }
+}
